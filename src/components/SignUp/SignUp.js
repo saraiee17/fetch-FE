abebdaefacfc4336abd459865e2453def7759ec6
Sarai Escobar <saraiee17@yahoo.com>
2023-01-20 14:19:35 -0600

    feat: form validation ensuring no empty fields: javascript,JSX, and styling

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,6 +4,8 @@ import axios from "axios";
 
 function SignUp(){
     const [formData, setFormData] = useState('');
+    const [states, setStates] = useState([]);
+    const [occupations, setOccupations] = useState([]);
 
       //Attempt to get request for formData to add to formInfo state.
       useEffect(() => {
@@ -11,16 +13,61 @@ function SignUp(){
         .get("https://frontend-take-home.fetchrewards.com/form")
         .then((response) => {
           setFormData(response.data);
+           //Manipulate formData to return occupations and states as individual arrays to map through
+        if (formData) {
+          setOccupations(Object.values(formData)[0])
+          setStates(Object.values(formData)[1].map(function(item){return item.name;}))
+         }
         })
         .catch((err) => {
           console.log(err);
         });
-    }, [])
+    }, [formData])
 
-        const occupations= Object.values(formData)[0]
-        const states=Object.values(formData)[1].map(function(item){return item.name;})
+    //Initialize formInfo as state variable to track changes
+    const [formInfo, setFormInfo] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        occupation: "",
+        state: "",
+    });
+  
+    //Initialize touched as state variable to track if a user has clicked in a form field
+    const [touched, setTouched] = useState({
+      first_name: false,
+      last_name: false,
+      email: false,
+      password: false,
+      occupation: false,
+      state: false,
+    });
+  
+    //Create error object to store errors in each input field field
+    let errors = {};
+  
+    //loop through formInfo. Set error at the given key to true
+    //IF there is an empty string in formInfo at the given key but it has been touched
+    for (const key in formInfo) {
+      errors[key] = !formInfo[key] && touched[key];
+    }
+  
+    //Updating touched value on click in an input field
+    const handleTouch = (event) => {
+      setTouched({ ...touched, [event.target.name]: true });
+    };
+  
+    //Track changes in input fields
+    const handleChange = (event) => {
+      setFormInfo({
+        ...formInfo,
+        [event.target.name]: event.target.value,
+      });
 
+    };
 
+console.log(errors)
     return(
     <div className='signup'>
              <form className="signup__form">
@@ -29,29 +76,29 @@ function SignUp(){
                 <div className='signup__tabletflex'>
                 <div className='signup__flex signup__left'>
                 <label className="signup__label" htmlFor='first_name' >First Name</label>
-                <input type="text" name="first_name" label="First name" />
+                <input onBlur={handleTouch} onChange={handleChange} className={`signup__input ${errors.first_name ? "signup__input--invalid" : ""}`} value={formInfo.first_name} type="text" id="first_name" name="first_name" label="First name" />
                 <label className="signup__label" htmlFor='last_name' >Last Name</label>
-                <input type="text" name="last_name" label="Last name" />
+                <input onBlur={handleTouch} onChange={handleChange} className={`signup__input ${errors.last_name ? "signup__input--invalid" : ""}`} value={formInfo.last_name} type="text" name="last_name" label="Last name" />
                 </div>
 
 
                 <div className='signup__flex'>
                 <label className="signup__label" htmlFor='email' >Email</label>
-                <input type="text" name="email" label="Email" />
+                <input onBlur={handleTouch} onChange={handleChange}className={`signup__input ${errors.email ? "signup__input--invalid" : ""}`} value={formInfo.email} type="text" name="email" label="Email" />
                 <label className="signup__label" htmlFor='password' >Password</label>
-                <input type="password" name="password" label="Password" />
+                <input onBlur={handleTouch} onChange={handleChange} className={`signup__input ${errors.password ? "signup__input--invalid" : ""}`} value={formInfo.password} type="password" name="password" label="Password" />
                 </div>
                 </div>
 
                 <label className='signup__label' htmlFor="occupation-list">Occupation</label>
-                <select className='signup__category' id='occupation-list' name='category' placeholder='{occupationdata}'>
+                <select onBlur={handleTouch} onChange={handleChange} className={`signup__input ${errors.occupation ? "signup__input--invalid" : ""}`} value={formInfo.occupation}  id='occupation-list' name='category' placeholder='{occupationdata}'>
                         {occupations.map((occupation,index)=>{
                      return <option key={index}className={occupation}>{occupation}</option>
                     })}
                 </select>
 
                 <label className='signup__label' htmlFor="state-list">State</label>
-                <select className='signup__category' id='state-list' name='category' placeholder='{statedata}'>
+                <select onBlur={handleTouch} onChange={handleChange}  className={`signup__input ${errors.state ? "signup__input--invalid" : ""}`} value={formInfo.state}  id='state-list' name='category' placeholder='{statedata}'>
                         {states.map((states,index)=>{
                      return <option key= {index}className={states}>{states}</option>
                     })}
@@ -59,8 +106,8 @@ function SignUp(){
 
                 <button className="signup__button">Sign up</button>
 
-                {/* {success && <div className="signup__message">Signed up!</div>}
-                {error && <div className="signup__message">{error}</div>} */}
+
+                {Object.values(errors).some((value) => value === true) && <div className="signup__message">Please Fill Out All Form Fields</div>}
             </form>
 
     </div>
